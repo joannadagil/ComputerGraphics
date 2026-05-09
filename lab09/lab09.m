0e0bfc7a8e6846ebf8c01cf9855ad9497868240c
asia dagil <asiadagil@gmail.com>
2026-05-09 23:18:47 +0200

    lab 9 doone

--- lab09/lab09.m
+++ lab09/lab09.m
@@ -10,25 +10,25 @@ clear; close all; clc;
 
 % Dane są cztery trójkąty (a dokładniej cztery ściany boczne ostrosłupa o podstawie kwadratowej) o następujących współrzędnych narożników:
 % Trójkąt 1:
-tri(1).A = ( 0;  0; 10);
-tri(1).B = (20; 20; 20);
-tri(1).C = (20;-20; 20);
+tri(1).A = [ 0;  0; 10];
+tri(1).B = [20; 20; 20];
+tri(1).C = [20;-20; 20];
 % Trójkąt 2:
-tri(2).A = tri(1).A
-tri(2).B = tri(1).C 
-tri(2).C = (-20; -20; 20);
+tri(2).A = tri(1).A;
+tri(2).B = tri(1).C;
+tri(2).C = [-20; -20; 20];
 % Trójkąt 3:
-tri(3).A = tri(1).A
-tri(3).B = tri(2).C
-tri(3).C = (-20; 20; 20);
+tri(3).A = tri(1).A;
+tri(3).B = tri(2).C;
+tri(3).C = [-20; 20; 20];
 % Trójkąt 4:
-tri(4).A = tri(1).A
+tri(4).A = tri(1).A;
 tri(4).B = tri(3).C;
 tri(4).C = tri(1).B;
 % Zwróć uwagę, że w sumie widzimy pięć różnych narożników.
 
 % Wszystkie trójkąty mają współczynnik odbicia 
-eps = 1;
+e = 1;
 
 % Scena ta wyświetlana jest na obrazie uzyskanym przez rzutowanie równoległe na płaszczyznę XY i zdyskretyzowanym do rozdzielczości XxY = 640x480
 X = 640;
@@ -42,14 +42,14 @@ dy = 0.1;
 amb = 20;
 
 % Dodatkowo scena oświetlona jest pojedynczym punktowym źródłem światła umieszczonym w
-lamp = [10,0,0];
+lamp = [10; 0; 0];
 % Natężenie źródła światła wynosi
 int = 235;
 % a spadek natężenia oświetlenia w zależności od odległości wynosi:
 % int / (1 + 0.001 || p - lamp ||^2
 
 % Jeśli potrzebne jest położenie „oka obserwatora”, to przyjmij
-eye = [65,0,0];
+eye = [65; 0; 0];
 
 
 
@@ -81,10 +81,10 @@ m = 15;
 
 Im = uint8(zeros(Y,X));
 
-Im = phong(Im, tri(1), amb, lamp, int, X, Y, dx, dy, e, m);
-Im = phong(Im, tri(2), amb, lamp, int, X, Y, dx, dy, e, m);
-Im = phong(Im, tri(3), amb, lamp, int, X, Y, dx, dy, e, m);
-Im = phong(Im, tri(4), amb, lamp, int, X, Y, dx, dy, e, m);
+Im = phong(Im, tri(1), amb, lamp, int, X, Y, dx, dy, e, eye, m);
+Im = phong(Im, tri(2), amb, lamp, int, X, Y, dx, dy, e, eye, m);
+Im = phong(Im, tri(3), amb, lamp, int, X, Y, dx, dy, e, eye, m);
+Im = phong(Im, tri(4), amb, lamp, int, X, Y, dx, dy, e, eye, m);
 
 figure(2); imshow(Im);
 
@@ -111,18 +111,68 @@ figure(3); imshow(Im);
 % Użyj oryginalnych wektorów normalnych w każdym narożniku trójkątów. 
 % Do obliczania oświetlenia narożników zastosuj model Lamberta.
 
+Im = uint8(zeros(Y,X));
+
+Im = gouraud_norm(Im, tri(1), amb, lamp, int, X, Y, dx, dy);
+Im = gouraud_norm(Im, tri(2), amb, lamp, int, X, Y, dx, dy);
+Im = gouraud_norm(Im, tri(3), amb, lamp, int, X, Y, dx, dy);
+Im = gouraud_norm(Im, tri(4), amb, lamp, int, X, Y, dx, dy);
+
+figure(4); imshow(Im);
+
 
 %% ZADANIE 5
 % Wygeneruj obraz tej sceny używając modelu cieniowania Gourauda. 
 % W narożnikach użyj wektorów normalnych, które są średnią wektorów normalnych trójkątów stykających się w danym narożniku. 
 % Do obliczania oświetlenia narożników zastosuj model Lamberta.
 
+Im = uint8(zeros(Y,X));
+
+ave = zeros(3,5);
+% 1: wierzchołek ostrosłupa, wspólny dla 4 trójkątów
+ave(:,1) = ave_vect([tri(1).A, tri(1).B, tri(1).C, ...
+                     tri(2).A, tri(2).B, tri(2).C, ...
+                     tri(3).A, tri(3).B, tri(3).C, ...
+                     tri(4).A, tri(4).B, tri(4).C], 4);
+
+% 2: tri(1).B = tri(4).C
+ave(:,2) = ave_vect([tri(1).A, tri(1).B, tri(1).C, ...
+                     tri(4).A, tri(4).B, tri(4).C], 2);
+
+% 3: tri(1).C = tri(2).B
+ave(:,3) = ave_vect([tri(1).A, tri(1).B, tri(1).C, ...
+                     tri(2).A, tri(2).B, tri(2).C], 2);
+
+% 4: tri(2).C = tri(3).B
+ave(:,4) = ave_vect([tri(2).A, tri(2).B, tri(2).C, ...
+                     tri(3).A, tri(3).B, tri(3).C], 2);
+
+% 5: tri(3).C = tri(4).B
+ave(:,5) = ave_vect([tri(3).A, tri(3).B, tri(3).C, ...
+                     tri(4).A, tri(4).B, tri(4).C], 2);
+
+
+Im = gouraud_spec(Im, tri(1), [ave(:,1), ave(:,2), ave(:,3)], amb, lamp, int, X, Y, dx, dy);
+Im = gouraud_spec(Im, tri(2), [ave(:,1), ave(:,3), ave(:,4)], amb, lamp, int, X, Y, dx, dy);
+Im = gouraud_spec(Im, tri(3), [ave(:,1), ave(:,4), ave(:,5)], amb, lamp, int, X, Y, dx, dy);
+Im = gouraud_spec(Im, tri(4), [ave(:,1), ave(:,5), ave(:,2)], amb, lamp, int, X, Y, dx, dy);
+
+figure(5); imshow(Im);
+
 
 %% ZADANIE 6
 % Wygeneruj obraz tej sceny używając modelu cieniowania Phonga. 
 % W narożnikach użyj wektorów normalnych, które są średnią wektorów normalnych trójkątów stykających się w danym narożniku. 
-% %Do obliczania oświetlenia punktów (pixeli) zastosuj model Lamberta.
+% Do obliczania oświetlenia punktów (pixeli) zastosuj model Lamberta.
 
+Im = uint8(zeros(Y,X));
+
+Im = phong_spec(Im, tri(1), [ave(:,1), ave(:,2), ave(:,3)], amb, lamp, int, X, Y, dx, dy);
+Im = phong_spec(Im, tri(2), [ave(:,1), ave(:,3), ave(:,4)], amb, lamp, int, X, Y, dx, dy);
+Im = phong_spec(Im, tri(3), [ave(:,1), ave(:,4), ave(:,5)], amb, lamp, int, X, Y, dx, dy);
+Im = phong_spec(Im, tri(4), [ave(:,1), ave(:,5), ave(:,2)], amb, lamp, int, X, Y, dx, dy);
+
+figure(6); imshow(Im);
 
 
 
@@ -144,11 +194,11 @@ function Im = lambert(Im, triangle, amb, lamp, int, X, Y, dx, dy)
     [i2,j2] = mat_to_pix(triangle.B(1), triangle.B(2), X, Y, dx, dy);
     [i3,j3] = mat_to_pix(triangle.C(1), triangle.C(2), X, Y, dx, dy);
 
-    Ish = line(Ish, j1, i1, j2, i2, 255);
-    Ish = line(Ish, j2, i2, j3, i3, 255);
-    Ish = line(Ish, j3, i3, j1, i1, 255);
+    Ish = line(Ish, j1, i1, j2, i2, dx, dy, 255);
+    Ish = line(Ish, j2, i2, j3, i3, dx, dy, 255);
+    Ish = line(Ish, j3, i3, j1, i1, dx, dy, 255);
 
-    Ish = floodfill1(Ish, round((j1+j2+j3)/3), round((i1+i2+i3)/3), 0, 255); % wypełnienie trójkąta białym kolorem
+    Ish = floodfill(Ish, round((j1+j2+j3)/3), round((i1+i2+i3)/3), 0, 255); % wypełnienie trójkąta białym kolorem
 
 
     [A, B, C, D] = plane(triangle.A, triangle.B, triangle.C); % Wyznaczenie parametrów płaszczyzny trójkąta
@@ -161,7 +211,7 @@ function Im = lambert(Im, triangle, amb, lamp, int, X, Y, dx, dy)
         for j = 1:Y
             if Ish(j,i) == 255 % Sprawdzenie, czy piksel jest wewnątrz trójkąta
                 [xx,yy] = pix_to_mat(i, j, X, Y, dx, dy); % Zamiana współrzędnych pikselowych na matematyczne
-                [~, x, y, z] = dist_to_plane(plane, xx, yy, 0, 0, 0, 1); % Wyznaczenie punktu na płaszczyźnie trójkąta
+                [~, x, y, z] = dist_to_plane([A,B,C,D], xx, yy, 0, 0, 0, 1); % Wyznaczenie punktu na płaszczyźnie trójkąta
                 l = lamp - [x; y; z]; % Wektor od punktu na trójkącie do lampy
                 dist = norm(l); % Odległość od lampy
                 l = l / dist; % Normalizacja wektora l
@@ -189,11 +239,11 @@ function Im = phong(Im, triangle, amb, lamp, int, X, Y, dx, dy, e, eye, m)
     [i2,j2] = mat_to_pix(triangle.B(1), triangle.B(2), X, Y, dx, dy);
     [i3,j3] = mat_to_pix(triangle.C(1), triangle.C(2), X, Y, dx, dy);
 
-    Ish = line(Ish, j1, i1, j2, i2, 255);
-    Ish = line(Ish, j2, i2, j3, i3, 255);
-    Ish = line(Ish, j3, i3, j1, i1, 255);
+    Ish = line(Ish, j1, i1, j2, i2, dx, dy, 255);
+    Ish = line(Ish, j2, i2, j3, i3, dx, dy, 255);
+    Ish = line(Ish, j3, i3, j1, i1, dx, dy, 255);
 
-    Ish = floodfill1(Ish, round((j1+j2+j3)/3), round((i1+i2+i3)/3), 0, 255); % wypełnienie trójkąta białym kolorem
+    Ish = floodfill(Ish, round((j1+j2+j3)/3), round((i1+i2+i3)/3), 0, 255); % wypełnienie trójkąta białym kolorem
     
     [A, B, C, D] = plane(triangle.A, triangle.B, triangle.C); % Wyznaczenie parametrów płaszczyzny trójkąta
     n = [A; B; C] / norm([A; B; C]); % Normalizacja wektora normalnego
@@ -205,7 +255,7 @@ function Im = phong(Im, triangle, amb, lamp, int, X, Y, dx, dy, e, eye, m)
         for j = 1:Y
             if Ish(j,i) == 255 % Sprawdzenie, czy piksel jest wewnątrz trójkąta
                 [xx,yy] = pix_to_mat(i, j, X, Y, dx, dy); % Zamiana współrzędnych pikselowych na matematyczne
-                [~, x, y, z] = dist_to_plane(plane, xx, yy, 0, 0, 0, 1); % Wyznaczenie punktu na płaszczyźnie trójkąta
+                [~, x, y, z] = dist_to_plane([A,B,C,D], xx, yy, 0, 0, 0, 1); % Wyznaczenie punktu na płaszczyźnie trójkąta
                 l = lamp - [x; y; z]; % Wektor od punktu na trójkącie do lampy
                 dist = norm(l); % Odległość od lampy
                 l = l / dist; % Normalizacja wektora l
@@ -243,15 +293,15 @@ function Im = const(Im, triangle, amb, lamp, int, X, Y, dx, dy)
     [i2,j2] = mat_to_pix(triangle.B(1), triangle.B(2), X, Y, dx, dy);
     [i3,j3] = mat_to_pix(triangle.C(1), triangle.C(2), X, Y, dx, dy);
 
-    Ish = line(Ish, j1, i1, j2, i2, 255);
-    Ish = line(Ish, j2, i2, j3, i3, 255);
-    Ish = line(Ish, j3, i3, j1, i1, 255);
+    Ish = line(Ish, j1, i1, j2, i2, dx, dy, 255);
+    Ish = line(Ish, j2, i2, j3, i3, dx, dy, 255);
+    Ish = line(Ish, j3, i3, j1, i1, dx, dy, 255);
 
     % srodek trojkata
     i0 = round((i1 + i2 + i3) / 3);
     j0 = round((j1 + j2 + j3) / 3);
 
-    Ish = floodfill(Ish, round((j1+j2+j3)/3), round((i1+i2+i3)/3), 0, 255); % wypełnienie trójkąta białym kolorem
+    Ish = floodfill(Ish, j0, i0, 0, 255); % wypełnienie trójkąta białym kolorem
 
 
     [A, B, C, D] = plane(triangle.A, triangle.B, triangle.C); % Wyznaczenie parametrów płaszczyzny trójkąta
@@ -260,11 +310,265 @@ function Im = const(Im, triangle, amb, lamp, int, X, Y, dx, dy)
         n = -1*n; 
     end % Obrócenie normalnej, jeśli jest skierowana w dół
 
+    [xx,yy] = pix_to_mat(i0, j0, X, Y, dx, dy);
+    [~, x, y, z] = dist_to_plane([A,B,C,D], xx, yy, 0, 0, 0, 1);
+    l = lamp - [x;y;z];
+    dist = norm(l);
+    l = l/dist;
+    cs = n'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    temp = amb + int * cs / (1+0.001*dist*dist);
+    if temp > 255 
+        temp = 255; 
+    end
+
     for i = 1:X
         for j = 1:Y
             if Ish(j,i) == 255 % Sprawdzenie, czy piksel jest wewnątrz trójkąta
+                Im(j, i) = temp; % Ustawienie natężenia dla pikseli wewnątrz trójkąta
+            end
+        end
+    end
+end
+
+
+%% GOURAUD
+
+function Im = gouraud_norm(Im, triangle, amb, lamp, int, X, Y, dx, dy)
+    Ish = uint8(zeros(Y,X)); 
+
+    [i1,j1] = mat_to_pix(triangle.A(1), triangle.A(2), X, Y, dx, dy);
+    [i2,j2] = mat_to_pix(triangle.B(1), triangle.B(2), X, Y, dx, dy);
+    [i3,j3] = mat_to_pix(triangle.C(1), triangle.C(2), X, Y, dx, dy);
+
+    Ish = line(Ish, j1, i1, j2, i2, dx, dy, 255);
+    Ish = line(Ish, j2, i2, j3, i3, dx, dy, 255);
+    Ish = line(Ish, j3, i3, j1, i1, dx, dy, 255);
+
+    % srodek trojkata
+    i0 = round((i1 + i2 + i3) / 3);
+    j0 = round((j1 + j2 + j3) / 3);
+
+    Ish = floodfill(Ish, j0, i0, 0, 255); % wypełnienie trójkąta białym kolorem
+
+    [A, B, C, D] = plane(triangle.A, triangle.B, triangle.C); % Wyznaczenie parametrów płaszczyzny trójkąta
+    n = [A; B; C] / norm([A; B; C]); % Normalizacja wektora normalnego
+    if n(3) > 0 
+        n = -1*n; 
+    end % Obrócenie normalnej, jeśli jest skierowana w dół
+
+    % kolejne narożniki
+
+    l = lamp-triangle.A;
+    dist = norm(l);
+    l = l/dist;
+    cs = n'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar1 = amb + int * cs / (1+0.001*dist*dist);
+    if nar1 > 255 
+        nar1 = 255; 
+    end
+    Im(j1,i1) = nar1;
+
+    l = lamp-triangle.B;
+    dist = norm(l);
+    l = l/dist;
+    cs = n'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar2 = amb + int * cs / (1+0.001*dist*dist);
+    if nar2 > 255 
+        nar2 = 255; 
+    end
+    Im(j2,i2) = nar2;
+
+    l = lamp-triangle.C;
+    dist = norm(l);
+    l = l/dist;
+    cs = n'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar3 = amb + int * cs / (1+0.001*dist*dist);
+    if nar3 > 255 
+        nar3 = 255; 
+    end
+    Im(j3,i3) = nar3;
+
+
+    for i = 1:X
+        for j = 1:Y
+            if Ish(j,i)==255
+                if isequal([j,i],[j1,i1]) continue; end
+                if isequal([j,i],[j2,i2]) continue; end
+                if isequal([j,i],[j3,i3]) continue; end
+                invd1 = 1/sqrt((j-j1)^2+(i-i1)^2); % wspolrzedne barycentryczne
+                invd2 = 1/sqrt((j-j2)^2+(i-i2)^2);
+                invd3 = 1/sqrt((j-j3)^2+(i-i3)^2);
+                Im(j,i) = (nar1*invd1+nar2*invd2+nar3*invd3)/(invd1+invd2+invd3);
+            end
+        end
+    end 
+end
+
+%% GOURAUD SPEC
+
+function Im = gouraud_spec(Im, triangle, ave, amb, lamp, int, X, Y, dx, dy)
+    Ish = uint8(zeros(Y,X));
+
+    [i1,j1] = mat_to_pix(triangle.A(1), triangle.A(2), X, Y, dx, dy);
+    [i2,j2] = mat_to_pix(triangle.B(1), triangle.B(2), X, Y, dx, dy);
+    [i3,j3] = mat_to_pix(triangle.C(1), triangle.C(2), X, Y, dx, dy);
+
+    Ish = line(Ish, j1, i1, j2, i2, dx, dy, 255);
+    Ish = line(Ish, j2, i2, j3, i3, dx, dy, 255);
+    Ish = line(Ish, j3, i3, j1, i1, dx, dy, 255);
+
+    % srodek trojkata
+    i0 = round((i1 + i2 + i3) / 3);
+    j0 = round((j1 + j2 + j3) / 3);
+
+    Ish = floodfill(Ish, j0, i0, 0, 255); % wypełnienie trójkąta białym kolorem
+
+    % kolejne narożniki
+    l = lamp-triangle.A;
+    dist = norm(l);
+    l = l/dist;
+    cs = ave(:,1)'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar1 = amb + int * cs / (1+0.001*dist*dist);
+    if nar1 > 255 
+        nar1 = 255; 
+    end
+    Im(j1,i1) = nar1;
+
+    l = lamp-triangle.B;
+    dist = norm(l);
+    l = l/dist;
+    cs = ave(:,2)'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar2 = amb + int * cs / (1+0.001*dist*dist);
+    if nar2 > 255 
+        nar2 = 255; 
+    end
+    Im(j2,i2) = nar2;
+
+    l = lamp-triangle.C;
+    dist = norm(l);
+    l = l/dist;
+    cs = ave(:,3)'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar3 = amb + int * cs / (1+0.001*dist*dist);
+    if nar3 > 255 
+        nar3 = 255; 
+    end
+    Im(j3,i3) = nar3;
+
+
+
+    for i = 1:X
+        for j = 1:Y
+            if Ish(j,i)==255
+                if isequal([j,i],[j1,i1]) continue; end
+                if isequal([j,i],[j2,i2]) continue; end
+                if isequal([j,i],[j3,i3]) continue; end
+                invd1 = 1/sqrt((j-j1)^2+(i-i1)^2); % wspolrzedne barycentryczne
+                invd2 = 1/sqrt((j-j2)^2+(i-i2)^2);
+                invd3 = 1/sqrt((j-j3)^2+(i-i3)^2);
+                Im(j,i) = (nar1*invd1 + nar2*invd2 + nar3*invd3) / (invd1+invd2+invd3);
+            end
+        end
+    end 
+end
+
+%% PHONG SPEC
+
+function Im = phong_spec(Im, triangle, ave, amb, lamp, int, X, Y, dx, dy)
+    Ish = uint8(zeros(Y,X));
+
+    [i1,j1] = mat_to_pix(triangle.A(1), triangle.A(2), X, Y, dx, dy);
+    [i2,j2] = mat_to_pix(triangle.B(1), triangle.B(2), X, Y, dx, dy);
+    [i3,j3] = mat_to_pix(triangle.C(1), triangle.C(2), X, Y, dx, dy);
+
+    Ish = line(Ish, j1, i1, j2, i2, dx, dy, 255);
+    Ish = line(Ish, j2, i2, j3, i3, dx, dy, 255);
+    Ish = line(Ish, j3, i3, j1, i1, dx, dy, 255);
+
+    % srodek trojkata
+    i0 = round((i1 + i2 + i3) / 3);
+    j0 = round((j1 + j2 + j3) / 3);
+
+    Ish = floodfill(Ish, j0, i0, 0, 255); % wypełnienie trójkąta białym kolorem
+
+    [A, B, C, D] = plane(triangle.A, triangle.B, triangle.C); % Wyznaczenie parametrów płaszczyzny trójkąta
+
+    % kolejne narożniki
+    l = lamp-triangle.A;
+    dist = norm(l);
+    l = l/dist;
+    cs = ave(:,1)'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar1 = amb + int * cs / (1+0.001*dist*dist);
+    if nar1 > 255 
+        nar1 = 255; 
+    end
+    Im(j1,i1) = nar1;
+
+    l = lamp-triangle.B;
+    dist = norm(l);
+    l = l/dist;
+    cs = ave(:,2)'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar2 = amb + int * cs / (1+0.001*dist*dist);
+    if nar2 > 255 
+        nar2 = 255; 
+    end
+    Im(j2,i2) = nar2;
+
+    l = lamp-triangle.C;
+    dist = norm(l);
+    l = l/dist;
+    cs = ave(:,3)'*l; 
+    if cs < 0 
+        cs = 0; 
+    end
+    nar3 = amb + int * cs / (1+0.001*dist*dist);
+    if nar3 > 255 
+        nar3 = 255; 
+    end
+    Im(j3,i3) = nar3;
+
+
+
+    for i = 1:X
+        for j = 1:Y
+            if Ish(j,i)==255
+                if isequal([j,i],[j1,i1]) continue; end
+                if isequal([j,i],[j2,i2]) continue; end
+                if isequal([j,i],[j3,i3]) continue; end
+                invd1 = 1/sqrt((j-j1)^2+(i-i1)^2); % wspolrzedne barycentryczne
+                invd2 = 1/sqrt((j-j2)^2+(i-i2)^2);
+                invd3 = 1/sqrt((j-j3)^2+(i-i3)^2);
+
+                n = (ave(:,1)*invd1 + ave(:,2)*invd2 + ave(:,3)*invd3) / (invd1+invd2+invd3);
+
                 [xx,yy] = pix_to_mat(i, j, X, Y, dx, dy); % Zamiana współrzędnych pikselowych na matematyczne
-                [~, x, y, z] = dist_to_plane(plane, xx, yy, 0, 0, 0, 1); % Wyznaczenie punktu na płaszczyźnie trójkąta
+                [~, x, y, z] = dist_to_plane([A,B,C,D], xx, yy, 0, 0, 0, 1); % Wyznaczenie punktu na płaszczyźnie trójkąta
                 l = lamp - [x; y; z]; % Wektor od punktu na trójkącie do lampy
                 dist = norm(l); % Odległość od lampy
                 l = l / dist; % Normalizacja wektora l
@@ -275,14 +579,32 @@ function Im = const(Im, triangle, amb, lamp, int, X, Y, dx, dy)
                 temp = amb + int * cs / (1 + 0.001 * dist^2); % Obliczenie natężenia oświetlenia z uwzględnieniem tłumienia
                 if temp > 255 
                     temp = 255; 
-                end % Ograniczenie natężenia do 255
+                end % Ograniczenie natężenia do 255 z uwzględnieniem tłumienia
                 Im(j, i) = temp; % Ustawienie natężenia dla pikseli wewnątrz trójkąta
             end
         end
-    end
+    end 
 end
 
 
+%% AVE VECT
+
+function ave = ave_vect(r,N)
+    % r: tablica naroznikow N stykajacych sie trojkatow
+    ave = [0;0;0];
+    for i = 0:N-1
+        r1(1:3) = r(1:3,3*i+1);
+        r2(1:3) = r(1:3,3*i+2);
+        r3(1:3) = r(1:3,3*i+3);
+        [A, B, C, ~] = plane(r1,r2,r3);
+        n = [A; B; C]/norm([A; B; C]); % wektor normalny
+        if n(3)>0 
+            n = -1*n; 
+        end % skierowany we wlasciwa strone
+        ave = ave+n;
+    end
+    ave = ave/norm(ave); % normalizacja
+end
 
 
 
